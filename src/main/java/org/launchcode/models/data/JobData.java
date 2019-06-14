a0d887ef4ff06733f797b38107e3048c6447e63d
farside1969 <farside1969@hotmail.com>
2019-06-14 15:57:08 -0500

    fixed lower case add search func

--- src/main/java/org/launchcode/models/data/JobData.java
+++ src/main/java/org/launchcode/models/data/JobData.java
@@ -62,7 +62,7 @@ public class JobData {
 
         for (Job job : jobs) {
 
-            if (job.getName().toLowerCase().contains(value)) {
+            if (job.getName().toLowerCase().contains(value.toLowerCase())) {
                 matchingJobs.add(job);
                 continue;
             }
